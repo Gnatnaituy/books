907a602363b003b6e3179a91211949ebade05e68
Arszrc <Arszrc@gmail.com>
2017-03-07 22:22:16 +0800

    aaa

---
+++ SICP/Chapter_2_Data_Abstraction/2Point2.scm
@@ -0,0 +1,43 @@
+(define one-through-four (list 1 2 3 4))
+(car one-through-four)
+(cdr one-through-four)
+
+(define (list-ref items n)
+    (if (= n 0)
+        (car items)
+        (list-ref (cdr items) (- n 1))))
+
+(define squares (list 1 4 9 16 25 36 49))
+
+(list-ref squares 3)
+
+(define (length items)
+    (if (null? items)
+        0
+        (+ 1 (length (cdr items)))))
+
+(define odds (list 1 3 5 7 9 11 13 15 17))
+(length odds)
+
+(define (append list1 list2)
+    (if (null? list1)
+        list2
+        (cons (car list1) (append (cdr list1) list2))))
+
+
+;;map the table
+(define (scale-list items factor)
+    (if (null? items)
+        nil
+        (cons (* (car items) factor)
+                  (scale-list (cdr items) factor))))
+
+(define (map proc items)
+    (if (null? items)
+         nil
+         (cons (proc (car items))
+                    (map proc (cdr items))))
+
+(map abs (list 1 2 -3 -4 -33.33))
+(map (lambda (x) (* x x))
+          (list 1 2 3 4 5 6 7 8 9))
