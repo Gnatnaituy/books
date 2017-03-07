1d894351d54166e3ae144082c9ad081e5a0b25c1
Arszrc <Arszrc@gmail.com>
2017-03-07 22:33:14 +0800

    2017/3/7

--- SICP/Chapter_2_Data_Abstraction/2Point2.scm
+++ SICP/Chapter_2_Data_Abstraction/2Point2.scm
@@ -1,3 +1,4 @@
+;;2017/3/7 Horo
 (define one-through-four (list 1 2 3 4))
 (car one-through-four)
 (cdr one-through-four)
@@ -41,3 +42,19 @@
 (map abs (list 1 2 -3 -4 -33.33))
 (map (lambda (x) (* x x))
           (list 1 2 3 4 5 6 7 8 9))
+
+;;Map the tree
+(define (scale-tree tree factor)
+    (cond ((null? tree) nil)
+              ((not (pair? tree)) (* tree factor))
+              (else (cons (scale-tree (car tree) factor)
+                                 (scale-tree (cdr tree) factor)))))
+
+(scale-tree (list 1 (list 2 (list 3 4) 5 ) (list 6 7)) 10)
+
+(define (scale-tree-2 tree factor)
+    (map (lambda (sub-tree)
+                    (if (pair? sub-tree)
+                        (scale-tree sub-tree factor)
+                        (* sub-tree factor)))
+              tree))
