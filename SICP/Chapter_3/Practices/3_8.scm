3f36ce76894584783530af47fe59b640fef6b279
Arszrc <Arszrc@gmail.com>
2017-04-09 21:46:00 +0800

    aaa

--- SICP/Chapter_3/Practices/3_8.scm
+++ SICP/Chapter_3/Practices/3_8.scm
@@ -1,7 +1,10 @@
+(define y 1)
 (define (f x)
-    (if (eq? x 0)
-        0
-        1))
+    (if (eqv? x y)
+        1
+        (begin (set! y 0) y)))
 
 (+ (f 0) (f 1))
+;; 0
 (+ (f 1) (f 0))
+;; 1
