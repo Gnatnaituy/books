25a3d11024e13b8efd3994a0b6edc852d4d658f8
Hasaker <Arszrc@gmail.com>
2017-11-13 22:04:45 +0800

    -a

--- K&R/main.c
+++ K&R/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-#include "Chapter_4/quick_sort.c"
+#include <memory.h>
 
 
 int main(void)
 {
-    int a[] = {2333, 123, 1, 5, 56, 2, 55, 789};
-    quick_sort(a, 0, 7);
+//    int a = -1, b = 4, k;
+//    k = !((++a < 0) && !(b-- < 0));
+//    printf("%d%d%d\n", k, a, b); //output: 104
+
+//    int i;
+//    i=1, ++i, ++i||++i, i;
+//    printf("%d", i);  //output: 3
+
+    char s[] = "\\141\141abc\t";
+    int i;
+    for(i = 0; i < strlen(s); i++)
+        printf("%c\n", s[i]);
 
     return 0;
 }
