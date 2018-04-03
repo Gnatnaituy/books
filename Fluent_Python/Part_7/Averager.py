9453968e8b001a0cd22461cdb4f5435bbacb2d43
Hasaker <hasaker@Hasakers-iMac.local>
2018-04-03 21:50:32 +0800

    aaa

---
+++ Fluent_Python/Part_7/Averager.py
@@ -0,0 +1,39 @@
+class Averager():
+    def __init__(self):
+        self.series = []
+
+    def __call__(self, new_value):
+        self.series.append(new_value)
+        total = sum(self.series)
+        return total / len(self.series)
+
+
+def make_averager():
+    series = []
+
+    def averager(new_value):
+        series.append(new_value)
+        total = sum(series)
+        return total / len(series)
+
+    return averager
+
+
+def make_averager2():
+    count = 0
+    total = 0
+
+    def averager(new_value):
+        nonlocal count, total
+        count += 1
+        total += new_value
+        return total / count
+
+    return averager
+
+
+if __name__ == '__main__':
+
+    avg = make_averager2()
+    print(avg(10))
+    print(avg(123))
