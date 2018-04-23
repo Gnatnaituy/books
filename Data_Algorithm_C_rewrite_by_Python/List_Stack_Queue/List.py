22b5b7cb8ae904f9086da5fdcad332919f8ba09c
Hasaker <Arszrc@gmail.com>
2018-04-23 23:05:59 +0800

    @property‘

--- Data_Algorithm_C_rewrite_by_Python/List_Stack_Queue/List.py
+++ Data_Algorithm_C_rewrite_by_Python/List_Stack_Queue/List.py
@@ -10,41 +10,89 @@ class SingleLinkedList:
             self._element = element
             self._next_node = next_node
 
+        @property
+        def element(self):
+            return self._element
+
+        @element.setter
+        def element(self, value):
+            self._element = value
+
+        @property
+        def next_node(self):
+            return self._next_node
+
+        @next_node.setter
+        def next_node(self, node):
+            if not isinstance(node, type(self)):
+                raise TypeError("Node type data accept only")
+            self._next_node = node
+
     def __init__(self):
         self._head = self._Node(None, None)
         self._tail = self._Node(None, None)
-        self._head._next_node = self._tail
+        self._head.next_node = self.tail
         self._length = 0
 
-    def __len__(self):
+    @property
+    def head(self):
+        return self._head
+
+    @head.setter
+    def head(self, node):
+        if not isinstance(node, type(self._Node)):
+            raise TypeError("Node type data accept only")
+        self._head = node
+
+    @property
+    def tail(self):
+        return self._tail
+
+    @tail.setter
+    def tail(self, node):
+        if not isinstance(node, type(self._Node)):
+            raise TypeError("Node type data accept only")
+        self._tail = node
+
+    @property
+    def length(self):
         return self._length
 
+    @length.setter
+    def length(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Int type data accept only")
+        self._length = value
+
+    def __len__(self):
+        return self.length
+
     def is_empty(self):
-        return self._length == 0
+        return self.length == 0
 
     def is_last(self):
-        return self._tail._next_node
+        return self.tail.next_node
 
     def find_previous(self, value):
-        p = self._head
-        while(p._next_node != self._tail and p._element != value):
-            p = p._next_node
+        p = self.head
+        while p.next_node != self.tail and p.element != value:
+            p = p.next_node
         return p
 
     def insert(self, previous_node, value):
-        new_node = self._Node(value, previous_node._next_node)
-        previous_node._next_node = new_node
-        self._length += 1
+        new_node = self._Node(value, previous_node.next_node)
+        previous_node.next_node = new_node
+        self.length += 1
 
     def append(self, value):
         new_node = self._Node(value, None)
-        self._tail._next_node = new_node
-        self._tail = new_node
-        self._length += 1
+        self.tail.next_node = new_node
+        self.tail = new_node
+        self.length += 1
 
 
 if __name__ == '__main__':
     my_list = SingleLinkedList()
-    for i in range(3):
+    for i in range(1, 4):
         my_list.append(str(i) * 9)
     print(my_list)
