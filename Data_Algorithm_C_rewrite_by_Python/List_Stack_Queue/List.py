8d5b351221719705a4658f03d988de6807ca8621
Hasaker <Arszrc@gmail.com>
2018-04-26 22:24:50 +0800

    双向链表

--- Data_Algorithm_C_rewrite_by_Python/List_Stack_Queue/List.py
+++ Data_Algorithm_C_rewrite_by_Python/List_Stack_Queue/List.py
@@ -31,64 +31,32 @@ class SingleLinkedList:
     def __init__(self):
         self._head = self._Node(None, None)
         self._tail = self._Node(None, None)
-        self._head.next_node = self.tail
+        self._head._next_node = self._tail
         self._length = 0
 
-    @property
-    def head(self):
-        return self._head
-
-    @head.setter
-    def head(self, node):
-        if not isinstance(node, type(self._Node)):
-            raise TypeError("Node type data accept only")
-        self._head = node
-
-    @property
-    def tail(self):
-        return self._tail
-
-    @tail.setter
-    def tail(self, node):
-        if not isinstance(node, type(self._Node)):
-            raise TypeError("Node type data accept only")
-        self._tail = node
-
-    @property
-    def length(self):
+    def __len__(self):
         return self._length
 
-    @length.setter
-    def length(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Int type data accept only")
-        self._length = value
-
-    def __len__(self):
-        return self.length
+    def _find_node(self, element):
+        this_node = self._head
+        while this_node.next_node is not self._tail and this_node.element != element:
+            this_node = this_node.next_node
+        return this_node
 
     def is_empty(self):
-        return self.length == 0
+        return self._length == 0
 
-    def is_last(self):
-        return self.tail.next_node
-
-    def find_previous(self, value):
-        p = self.head
-        while p.next_node != self.tail and p.element != value:
-            p = p.next_node
-        return p
-
-    def insert(self, previous_node, value):
-        new_node = self._Node(value, previous_node.next_node)
-        previous_node.next_node = new_node
-        self.length += 1
+    def insert_after(self, element, value):
+        the_node = self._find_node(element)
+        new_node = self._Node(value, the_node.next_node)
+        the_node.next_node = new_node
+        self._length += 1
 
     def append(self, value):
         new_node = self._Node(value, None)
-        self.tail.next_node = new_node
-        self.tail = new_node
-        self.length += 1
+        self._tail.next_node = new_node
+        self._tail = new_node
+        self._length += 1
 
 
 class DoublyLinkedList:
@@ -135,65 +103,40 @@ class DoublyLinkedList:
     def __init__(self):
         self._head = self._Node(None, None, None)
         self._tail = self._Node(None, None, None)
+        self._head.next_node = self._tail
+        self._tail.previous_node = self._head
         self._length = 0
 
-    @property
-    def tail(self):
-        return self._tail
-
-    @tail.setter
-    def tail(self, node):
-        if not isinstance(node, type(self)):
-            raise TypeError("Node type data accept only")
-        self._tail = node
-
-    @property
-    def head(self):
-        return self._head
-
-    @head.setter
-    def head(self, node):
-        if not isinstance(node, type(self)):
-            raise TypeError("Node type data accept only")
-        self._head = node
-
-    @property
-    def length(self):
+    def __len__(self):
         return self._length
 
-    @length.setter
-    def length(self, value):
-        if not isinstance(value, int):
-            raise ValueError("Int type data accept only")
-        self._length = value
-
-    def __len__(self):
-        return self.length
+    def _find_node(self, element):
+        this_node = self._head
+        while this_node.next_node is not self._tail and this_node.element != element:
+            this_node = this_node.next_node
+        return this_node
 
     def is_empty(self):
-        return self.length == 0
+        return self._length == 0
 
-    def is_head(self):
-        pass
-
-    def is_tail(self):
-        pass
-
-    def find_previous(self):
-        pass
-
-    def find_next(self):
-        pass
-
-    def insert_between(self, previous_node, next_node, value):
-        pass
+    def insert_between(self, previous_element, next_element, value):
+        previous_node = self._find_node(previous_element)
+        next_node = self._find_node(next_element)
+        new_node = self._Node(value, previous_node, next_node)
+        previous_node.next_node = new_node
+        next_node.previous_node = new_node
+        self._length += 1
 
     def append(self, value):
-        pass
+        new_node = self._Node(value, self._tail, None)
+        self._tail.next_node = new_node
+        self._tail = new_node
+        self._length += 1
 
 
 if __name__ == '__main__':
-    my_list = SingleLinkedList()
-    for i in range(1, 4):
+    my_list = DoublyLinkedList()
+    for i in range(1, 6):
         my_list.append(str(i) * 9)
+    my_list.insert_between('2' * 9, '3' * 9, '0' * 9)
     print(my_list)
