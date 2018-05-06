126751a5e2a6fb2038604aeb330a9139722a1d67
Hasaker <Arszrc@gmail.com>
2018-05-06 17:16:02 +0800

    No feeling...........

---
+++ Data_Algorithm_C_rewrite_by_Python/List_Stack_Queue/Stack.py
@@ -0,0 +1,19 @@
+class Stack:
+
+    class _Node:
+
+        __slots__ == '_element'
+
+        def __init__(self, element, next_node):
+            self._element = element
+            self._next_node = next_node
+
+        @property
+        def element(self):
+            return self._element
+
+        @element.setter
+        def element(self, element):
+            self._element = element
+        
+        
