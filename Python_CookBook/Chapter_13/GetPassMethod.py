29001e5927ec3b034319fe96c6d91a593397e127
Hasaker <Arszrc@gmail.com>
2018-07-18 20:24:21 +0800

    Don't what to do

--- Python_CookBook/Chapter_13/GetPassMethod.py
+++ Python_CookBook/Chapter_13/GetPassMethod.py
@@ -3,11 +3,13 @@ import getpass
 user = input('Enter your username: ')
 passwd = getpass.getpass()
 
+
 def svc_login(user, passwd):
     if passwd == '5523':
         return True
     else:
         return False
+
 
 if svc_login(user, passwd):
     print('Yay!')
--- Python_CookBook/Chapter_13/search.py
+++ Python_CookBook/Chapter_13/search.py
@@ -1,14 +1,16 @@
 # search.py
-'''
+
+"""
 Hypothetical command-line tool for searching a collection of
 files for one or more text patterns.
-'''
+"""
+
 import argparse
 parser = argparse.ArgumentParser(description='Search some files')
 
-parser.add_argument(dest='filenames',metavar='filename', nargs='*')
+parser.add_argument(dest='filenames', metavar='filename', nargs='*')
 
-parser.add_argument('-p', '--pat',metavar='pattern', required=True,
+parser.add_argument('-p', '--pat', metavar='pattern', required=True,
                     dest='patterns', action='append',
                     help='text pattern to search for')
 
@@ -19,7 +21,7 @@ parser.add_argument('-o', dest='outfile', action='store',
                     help='output file')
 
 parser.add_argument('--speed', dest='speed', action='store',
-                    choices={'slow','fast'}, default='slow',
+                    choices={'slow', 'fast'}, default='slow',
                     help='search speed')
 
 args = parser.parse_args()
